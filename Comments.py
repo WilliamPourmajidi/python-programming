ad4584011090d7951d43394e5a5d35dc6dea71de
williamPourmajidi <william.pourmajidi@gmail.com>
2026-01-24 11:02:04 -0500

    Adding code for comments

---
+++ Comments.py
@@ -0,0 +1,22 @@
+# comments are not executed
+# these are notes for Human rather than machines! 
+
+print("This is a command that will be executed!") # This is an inline comment
+
+"""
+This is a multiple line comment -- Appraoch #1
+print("Hello")
+print("Hello World, second line!")
+print("Hello World, third line!")
+print("Hello World, fourth line!")
+print("Hello World, fifth line!")
+"""
+ 
+# This is a multiple line comment -- Appraoch #2
+# print("Hello")
+# print("Hello World, second line!")
+# print("Hello World, third line!")
+# print("Hello World, fourth line!")
+# print("Hello World, fifth line!")
+ 
+
